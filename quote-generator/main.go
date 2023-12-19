0f4fc3383c7e3fc43c6449314420667c55cc91c5
rowinf <robert.irwin@outlook.co.nz>
2023-12-18 22:03:29 -0300

    json/html webserver for quote fetcher

--- quote-generator/main.go
+++ quote-generator/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"text/template"
 )
 
 type Quote struct {
@@ -12,29 +14,91 @@ type Quote struct {
 	Author string `json:"a"`
 }
 
-func main() {
+func fetchRandomQuote() []Quote {
+	var quotes []Quote
 	resp, err := http.Get("https://zenquotes.io/api/random")
 	if err != nil {
 		fmt.Println("Failed to fetch quote:", err)
-		return
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Failed to read response body:", err)
-		return
 	}
-
-	var quotes []Quote
 	err = json.Unmarshal(body, &quotes)
 	if err != nil {
 		fmt.Println("Failed to parse response body:", err)
+	}
+
+	return quotes
+}
+
+// Function to render JSON response
+func renderJSON(w http.ResponseWriter, data any) {
+	// Convert the data to JSON
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, "Failed to marshal data to JSON", http.StatusInternalServerError)
 		return
 	}
 
-	for _, quote := range quotes {
-		fmt.Println(quote.Quote)
-		fmt.Println("  - ", quote.Author)
+	// Set the response headers
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON response
+	w.Write(jsonData)
+}
+
+// Function to render HTML response
+func renderHTML(w http.ResponseWriter, data any) {
+	// Create an HTML template
+	htmlTemplate := `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Data Page</title>
+		</head>
+		<body>
+			<blockquote>{{.Quote}}<span>{{.Author}}</span></blockquote>
+		</body>
+		</html>
+	`
+
+	// Parse the HTML template
+	tmpl, err := template.New("html").Parse(htmlTemplate)
+	if err != nil {
+		http.Error(w, "Failed to parse HTML template", http.StatusInternalServerError)
+		return
 	}
+
+	// Set the response header
+	w.Header().Set("Content-Type", "text/html")
+
+	// Execute the template with the data and write the HTML response
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Failed to execute HTML template", http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/api/data", func(w http.ResponseWriter, r *http.Request) {
+		data := fetchRandomQuote()
+
+		// Check the "Accept" header to determine the response format
+		acceptHeader := r.Header.Get("Accept")
+		if strings.Contains(acceptHeader, "text/html") {
+			// Respond with HTML
+			renderHTML(w, data)
+		} else {
+			// Respond with JSON
+			renderJSON(w, data)
+		}
+	})
+
+	// Start the HTTP server on port 8080
+	fmt.Println("Server is running on :8080")
+	http.ListenAndServe(":8080", nil)
 }
