053b4c62d47d2bb0df7759ecdef8b09ea0047e74
rowinf <robert.irwin@outlook.co.nz>
2023-12-21 16:06:31 -0300

    add echo framework instead of templ

--- infinity-scroll/main.go
+++ infinity-scroll/main.go
@@ -1,31 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"io"
 	"net/http"
 	"text/template"
+
+	"github.com/labstack/echo/v4"
 )
 
-// Function to render HTML response
-func renderPhotos(w http.ResponseWriter, data any) {
-	tmpl, err := template.ParseFiles("index.html")
-	if err != nil {
-		log.Fatal(err)
-		http.Error(w, "Failed to parse HTML template", http.StatusInternalServerError)
-		return
-	}
+type Template struct {
+	templates *template.Template
+}
 
-	// Set the response header
-	w.Header().Set("Content-Type", "text/html")
-
-	// Execute the template with the data and write the HTML response
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		log.Fatal(err)
-		http.Error(w, "Failed to execute HTML template", http.StatusInternalServerError)
-		return
-	}
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func fileHandler(filename string) func(http.ResponseWriter, *http.Request) {
@@ -43,19 +31,33 @@ type PageData struct {
 	Images []ImageType
 }
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		images := []ImageType{
-			{"https://images.unsplash.com/photo-1702998033114-c01f9b2dea5b?q=80&w=365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-			{"https://images.unsplash.com/photo-1702998033114-c01f9b2dea5b?q=80&w=365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-		}
-		Index("Page Title", images).Render(r.Context(), w)
-	})
+func indexHandler(c echo.Context) error {
+	images := []ImageType{
+		{"https://images.unsplash.com/photo-1702998033114-c01f9b2dea5b?q=80&w=365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+		{"https://images.unsplash.com/photo-1702998033114-c01f9b2dea5b?q=80&w=365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+		{"https://images.unsplash.com/photo-1702998033114-c01f9b2dea5b?q=80&w=365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+	}
+	pageData := PageData{"Page Title", images}
 
-	http.HandleFunc("/style.css", fileHandler("style.css"))
-	fmt.Println("Server is running on :3000")
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Fatal("Could not start server ", err)
+	if err := c.Render(http.StatusOK, "index.html", pageData); err != nil {
+		c.Logger().Fatal(err)
+		return err
+	}
+	return nil
+}
+
+func main() {
+	e := echo.New()
+	e.File("/style.css", "style.css")
+	t := &Template{
+		templates: template.Must(template.ParseFiles("index.html")),
+	}
+	e.Renderer = t
+
+	e.GET("/", indexHandler)
+
+	e.Logger.Info("Server is running on http://localhost:3000")
+	if err := e.Start(":3000"); err != nil {
+		e.Logger.Fatal("Could not start server ", err)
 	}
 }
