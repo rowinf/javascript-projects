2f0fac38c88e795b3908978a46bd17adfb4c3668
rowinf <robert.irwin@outlook.co.nz>
2023-12-22 08:13:21 -0300

    implement infinite scrolling on image list

--- infinity-scroll/main.go
+++ infinity-scroll/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"text/template"
 
 	"github.com/labstack/echo/v4"
@@ -19,43 +21,54 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 }
 
 type PageData struct {
-	Title  string
-	Images []Image
+	ImagePage int
+	Images    []Image
 }
 
-// Image represents image data from Unsplash
 type Image struct {
 	Urls struct {
 		Small string `json:"small"`
 	} `json:"urls,omitempty"`
 	Caption string `json:"alt_description,omitempty"`
+	Links   struct {
+		Html string `json:"html"`
+	} `json:"links,omitempty"`
 }
 
-func fetchImages() ([]Image, error) {
+func NewPageData(page string) PageData {
+	i, err := strconv.Atoi(page)
+	if err != nil {
+		i = 1
+	}
+	return PageData{i, make([]Image, 10)}
+}
+
+func fetchImages(pageData PageData) error {
 	unsplashAPI := "https://api.unsplash.com/photos"
 	accessKey := os.Getenv("UNSPLASH_API_KEY")
-	response, err := http.Get(unsplashAPI + "?client_id=" + accessKey)
+	if accessKey == "" {
+		panic("uninitialised env accessKey")
+	}
+	response, err := http.Get(unsplashAPI + "?client_id=" + accessKey + "&page=" + fmt.Sprintf("%d", pageData.ImagePage))
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer response.Body.Close()
 
-	var images []Image
-	err = json.NewDecoder(response.Body).Decode(&images)
+	err = json.NewDecoder(response.Body).Decode(&pageData.Images)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return images, nil
+	return nil
 }
 
 func indexHandler(c echo.Context) error {
-	images, uerr := fetchImages()
+	pageData := NewPageData(c.QueryParam("page"))
+	uerr := fetchImages(pageData)
 	if uerr != nil {
 		c.Logger().Warn("couldn't fetch images", uerr)
 	}
-
-	pageData := PageData{"Page Title", images}
 
 	if err := c.Render(http.StatusOK, "index.html", pageData); err != nil {
 		c.Logger().Fatal(err)
@@ -64,16 +77,37 @@ func indexHandler(c echo.Context) error {
 	return nil
 }
 
+func imageListHandler(c echo.Context) error {
+	pageData := NewPageData(c.QueryParam("page"))
+	uerr := fetchImages(pageData)
+	if uerr != nil {
+		c.Logger().Warn("couldn't fetch images", uerr)
+	}
+
+	if err := c.Render(http.StatusOK, "image-list.html", pageData); err != nil {
+		c.Logger().Fatal(err)
+		return err
+	}
+	return nil
+}
+
+var fns = template.FuncMap{
+	"plus1": func(x int) int {
+		return x + 1
+	},
+}
+
 func main() {
 	e := echo.New()
 	e.File("/style.css", "style.css")
 	e.File("/script.js", "script.js")
 	t := &Template{
-		templates: template.Must(template.ParseFiles("index.html")),
+		templates: template.Must(template.New("index.html").Funcs(fns).ParseFiles("index.html", "image-list.html")),
 	}
 	e.Renderer = t
 
 	e.GET("/", indexHandler)
+	e.GET("/image-list", imageListHandler)
 
 	e.Logger.Info("Server is running on http://localhost:3000")
 	if err := e.Start(":3000"); err != nil {
