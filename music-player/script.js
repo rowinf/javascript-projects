d0a64736ab1e1dc4f44022650aa321cb1de2580a
rowinf <>
2024-01-09 23:23:01 +1300

    add progress bar

--- music-player/script.js
+++ music-player/script.js
@@ -1,3 +1,8 @@
+const progress = document.getElementById("progress");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
+const progressContainer = document.getElementById("progress-container");
+
 const playBtn = document.getElementById("play-button");
 const audio = document.querySelector("audio");
 const image = document.querySelector("img");
@@ -8,6 +13,11 @@ const songs = [
   {
     name: "jacinto-1",
     displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
     artist: "Jacinto Design",
   },
   {
@@ -23,6 +33,7 @@ const songs = [
 ];
 
 let isPlaying = false;
+let songIndex = 0;
 
 function toggleSong() {
   isPlaying = !isPlaying;
@@ -39,3 +50,56 @@ function toggleSong() {
   }
 }
 
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  audio.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) songIndex = songs.length - 1;
+  loadSong(songs[songIndex]);
+  isPlaying = false;
+  toggleSong();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) songIndex = 0;
+  loadSong(songs[songIndex]);
+  isPlaying = false;
+  toggleSong();
+}
+
+loadSong(songs[0]);
+
+audio.addEventListener("timeupdate", function updateProgressBar(event) {
+  if (isPlaying) {
+    const { duration, currentTime } = event.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    const durationMinutes = Math.floor(duration / 60);
+
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) durationSeconds = `0${durationSeconds}`;
+    if (durationSeconds)
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    const currentMinutes = Math.floor(currentTime / 60);
+
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) currentSeconds = `0${currentSeconds}`;
+    if (currentSeconds)
+      currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  }
+});
+
+progressContainer.addEventListener("click", function setProgressBar(event) {
+  const clickX = event.offsetX;
+  const width = this.clientWidth;
+  const { duration } = audio;
+  console.log(duration * (clickX / width));
+  audio.currentTime = duration * (clickX / width);
+  // progress.style.width = `${percent}%`;
+});
