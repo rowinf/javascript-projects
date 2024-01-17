c74b4cb2a1fecca95e0817bd3566eb01501d7993
rowinf <>
2024-01-17 16:21:17 +1300

    add form field validation

--- book-keeper/script.js
+++ book-keeper/script.js
@@ -1,14 +1,43 @@
-const bookmarkForm = document.getElementById('bookmark-form')
-const websiteNamEl = document.getElementById('website-name')
-const websiteUrlEl = document.getElementById('website-url')
-const bookmarksContainer = document.getElementById('bookmarks-container')
-const modalShow = document.getElementById('show-modal')
-const modal = document.getElementById('modal')
-const modalClose = document.getElementById('close-modal')
+const bookmarkForm = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarksContainer = document.getElementById("bookmarks-container");
+const modalShow = document.getElementById("show-modal");
+const modal = document.getElementById("modal");
+const modalClose = document.getElementById("close-modal");
 
-function showModal () {
-  modal.classList.add('show-modal')
-  websiteNamEl.focus()
+function showModal() {
+  modal.classList.add("show-modal");
+  websiteNameEl.focus();
 }
 
-modalShow.addEventListener('click', showModal)
+modalShow.addEventListener("click", showModal);
+modalClose.addEventListener("click", () =>
+  modal.classList.remove("show-modal"),
+);
+window.addEventListener("click", (e) => {
+  if (e.target.id === "modal") modal.classList.remove("show-modal");
+});
+
+const validateUrl = (str) => {
+  const urlRegex = new RegExp(
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+  );
+  return str.match(urlRegex);
+};
+
+bookmarkForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  let invalidUrl = false;
+  if (urlValue.search(/https?:\/\//) === -1) {
+    invalidUrl = !validateUrl(`https://${urlValue}`);
+  } else {
+    invalidUrl = !validateUrl(urlValue);
+  }
+  if (invalidUrl) {
+    alert("invalid url");
+  }
+  console.log(nameValue, urlValue);
+});
